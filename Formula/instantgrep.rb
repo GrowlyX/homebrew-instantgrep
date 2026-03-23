f8c616bd791fe206c0a8406746d2c7664bdec5b5
Subham K. <me@growly.gg>
2026-03-23 11:38:11 -0700

    fix: correct SHA256 checksums for v0.0.1

--- Formula/instantgrep.rb
+++ Formula/instantgrep.rb
@@ -1,27 +1,20 @@
 class Instantgrep < Formula
   desc "Fast regex search using trigram indexes"
   homepage "https://github.com/GrowlyX/instantgrep"
-  version "0.0.2"
+  version "0.0.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/GrowlyX/instantgrep/releases/download/v0.0.2/instantgrep-darwin-arm64.tar.gz"
-      sha256 "Not"
+      url "https://github.com/GrowlyX/instantgrep/releases/download/v0.0.1/instantgrep-darwin-arm64.tar.gz"
+      sha256 "395582b0b72f0db4722177543e1ccaf71a3ffcad648ba598e1b87ecceb107384"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/GrowlyX/instantgrep/releases/download/v0.0.2/instantgrep-linux-amd64.tar.gz"
-      sha256 "Not"
-    end
-  end
-
-  on_system :windows do
-    on_intel do
-      url "https://github.com/GrowlyX/instantgrep/releases/download/v0.0.2/instantgrep-windows-amd64.tar.gz"
-      sha256 "Not"
+      url "https://github.com/GrowlyX/instantgrep/releases/download/v0.0.1/instantgrep-linux-amd64.tar.gz"
+      sha256 "6bfedd8a54ad0debf7ef544b584b69910edeb71729a2d1f9f92da61cf971658e"
     end
   end
 
